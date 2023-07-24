3b5c15376108f74f0e8828ef7a5740c417568291
Your Name <you@example.com>
2023-07-24 17:41:02 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask ,render_template , request ,session, redirect
+from models import MyMongo
+from config import MONGODB_URL
+from functools import wraps
+app = Flask(__name__)
+app.secret_key = "a"
+mymongo = MyMongo(MONGODB_URL, 'os')
+
+def is_loged_in(func):
+    @wraps(func)
+    def wrap(*args , **kwargs):
+        if 'is_loged' in session:
+            return func(*args , **kwargs)
+        else:
+            return redirect('/login')
+    return wrap
+
+def is_admin(f):
+    @wraps(f)
+    def wrap(*args , **kwargs):
+        if session['username'] == "admin":
+            return f(*args , **kwargs)
+        else:
+            return redirect('/login')
+    return wrap
+
+@app.route('/', methods=['GET','POST'])
+def main():
+    return render_template('index.html')
+
+@app.route('/admin', methods=['GET', 'POST'])
+@is_loged_in
+@is_admin
+def admin():
+    return render_template('admin.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method =="POST":
+        username = request.form['username']
+        email = request.form['email']
+        phone = request.form['phone']
+        password = request.form['password']
+        user = mymongo.find_user(email)
+        if user:            
+            return redirect('/register')
+        else:
+            if username =="admin":
+                return redirect('/register')
+            # print(type(password))
+            else:      
+                result =  mymongo.user_insert(username,email,phone,password)
+                return redirect('/login')
+        
+    elif request.method =="GET":
+        return render_template('register.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    elif request.method == 'POST':
+        email = request.form['email']
+        print(email)
+        password = request.form['password']
+        result=mymongo.verify_password(password, email)
+        user = mymongo.find_user(email)
+        # print(f'fwefwfwef{user}')
+        if result =="1":
+            session['is_loged'] = True
+            session['username'] = user['username']
+            return render_template('index.html', message=user)
+        elif result =="2":
+            return render_template("login.html", message="Wrong")
+        elif result =='3':
+            return render_template("register.html" , message='None')
+
+@app.route('/create', methods=['GET','POST'])
+@is_loged_in
+def create():  
+    if request.method == 'GET':
+        return render_template('create.html')
+    else:
+        title = request.form['title']
+        desc = request.form['desc']  
+        author = request.form['author']
+        # print(author) 
+        # return 'a'    
+        result = mymongo.insert_data(title,desc,author) 
+        return redirect('/list')  
+          
+@app.route('/list')
+def list():
+    data = mymongo.find_data()
+    # for i in data:
+    #     print(i)
+    return render_template('list.html' , data = data)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+if __name__== "__main__":
+    app.run(debug=True, port=9999)
---
+++ models.py
@@ -0,0 +1,127 @@
+from pymongo import MongoClient
+import datetime
+
+# 암호화 알고리즘. 256을 제일 많이 사용한다.
+from passlib.hash import pbkdf2_sha256
+from bson.objectid import ObjectId
+from config import MONGODB_URL
+
+def hash_password(original_password):
+    salt = 'eungok'
+    password = original_password + salt
+    password = pbkdf2_sha256.hash(password)
+    return password
+
+def check_password(input_password, hashed_password):
+    salt = 'eungok'
+    password = input_password + salt
+    result = pbkdf2_sha256.verify(password , hashed_password)
+    return result
+
+class MyMongo:
+
+    def __init__(self , db_url , database):
+        self.database = database
+        self.client = MongoClient(db_url)
+
+    def user_insert(self , username, email, phone, password):
+        db = self.client.os
+        users = db.users
+        pw = hash_password(password)
+        user = {
+            "username":username,
+            "email":email,
+            "phone":phone,
+            "password":pw,
+            "create_at":datetime.datetime.utcnow()
+        }
+        result = users.insert_one(user)
+        print(result)
+        return 1
+    
+    def verify_password(self,input_password,  email):
+        db = self.client.os
+        users = db.users
+        user = users.find_one({'email':email})
+        # print(user)
+        if user:
+            result = check_password(input_password , user['password'])
+            if result:
+                print("Verify Success")
+                return "1"
+            else:
+                print("Verify Fail")
+                return "2"
+        else:
+            print("id isnot Founded")
+            return "3"
+    
+    def find_user(self, email):
+        db = self.client.os
+        users = db.users
+        user = users.find_one({'email':email})
+        print(type(user))
+        return user
+    
+    def find_data(self):
+        db = self.client.os
+        lists = db.lists
+        list = lists.find()
+        # for i in list:
+        #     print(i)
+        return list
+    
+    def insert_data(self, title, desc, author):
+        db = self.client.os
+        lists = db.lists
+        data = {
+            'title':title,
+            'desc':desc,
+            'author':author,
+            'create_at':datetime.datetime.utcnow()
+        }      
+        list = lists.insert_one(data)
+        # for i in list:
+        #     print(i)
+        return list
+# mymongo = MyMongo(MONGODB_URL , 'os')
+# # mymongo.user_insert("KIM", "2@naver.com", "010-1111-1111", "1234")
+# mymongo.verify_password("12343232",ObjectId('64ba2af7b26ddf90ce9642ac'))
+# mymongo.find_user("1@naver.com")
+# mymongo.find_data()
+
+# 원문 비밀번호를, 암호화 하는 함수
+
+
+# def verify_password(self, password, hashed_password):        
+#         result = check_password(password, hashed_password)
+#         return result
+# # 방법1 - URI
+# mongodb_URI = MONGODB_URL
+# client = MongoClient(mongodb_URI)
+
+# # print(client.list_database_names())
+
+# db = client.os
+# users = db.users
+# result = hash_password('1234')
+# user = {
+#     "username":"김태경",
+#     "email":"1@naver.com",
+#     "phone":"010-0000-0000",
+#     "password":result,
+#     "create_at":datetime.datetime.utcnow()
+# }
+
+
+# # result = users.insert_one(user)
+
+# # print(result)
+
+# # user = users.find()
+# # continents = [continent for continent in user]
+# # print(continents[0]['_id'])
+
+# user = users.find_one({'_id':ObjectId('64ba22e3b07d1e5e567fae07')})
+# print(user)
+
